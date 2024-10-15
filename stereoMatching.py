65b389ffbc92b28456a74d22ef32c2d63071728c
Lobke Stienstra <l.c.w.stienstra@student.utwente.nl>
2024-10-15 12:35:55 +0200

    stereoMatching.py

--- stereoMatching.py
+++ stereoMatching.py
@@ -22,23 +22,9 @@ def stereoMatching(img1,img2,h,w):
             # find the best match
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-            print(max_loc[0])
+            # print(max_loc[0])
             for i in range(scanH,min(scanH+h, imgH),1):
                 for j in range(scanW,min(scanW+w, imgW),1):
                     DMap[i,j] = max_loc[0]-scanW
     return DMap
 
-img1 = cv.imread('rectified1.jpg')
-img2 = cv.imread('rectified2.jpg')
-DMap = stereoMatching(img1,img2,16,16)
-
-DMap_clipped = np.clip(DMap, 0, 255)
-# 使用 imshow 显示矩阵
-plt.imshow(DMap, cmap='viridis', interpolation='nearest')  # 选择 'viridis' 颜色映射
-plt.colorbar()  # 添加颜色条
-
-# 保存图片到文件
-plt.savefig('DMap.png')
-
-# 显示图片
-plt.show()
