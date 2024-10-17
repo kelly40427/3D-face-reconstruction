7cc4bd53d65f8a41dca86a2fdc59e0b7bf98b52c
kelly40427 <kelly40427kelly40427@gmail.com>
2024-10-17 22:47:05 +0200

    create pcd and ICP

--- DepthMapCreator.py
+++ DepthMapCreator.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import open3d as o3d
 
 class DepthMapCreator:
 
@@ -38,3 +39,32 @@ class DepthMapCreator:
         plt.title('Depth Map')
         plt.savefig(output_path)
         plt.show()
+
+    def create_3d_points(self, disparity_map, Q):
+
+        points_3d = cv2.reprojectImageTo3D(disparity_map, Q)
+
+        # 建立有效視差範圍的遮罩
+        min_disparity = 10
+        max_disparity = 64
+        valid_mask = (disparity_map > min_disparity) & (disparity_map < max_disparity)
+
+        # 同時使用遮罩篩選 points_3D
+        points_3d = points_3d[valid_mask]
+          
+        return points_3d, valid_mask
+    
+    def create_3dpoint_cloud(self, points_3d, colors):
+        """
+        Create an Open3D point cloud object from 3D points and colors.
+        """
+
+        # Create Open3D point cloud object
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points_3d.reshape(-1, 3))  # Reshape points into Nx3
+        # Normalize the color values (if not already) and assign to point cloud
+        pcd.colors = o3d.utility.Vector3dVector(colors/ 255.0)  # Reshape and normalize colors
+        pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+        print("Number of points in point cloud:", np.asarray(pcd.points).shape[0])
+    
+        return pcd
--- main_code.py
+++ main_code.py
@@ -5,6 +5,9 @@ import os
 import matplotlib.pyplot as plt
 from PIL import Image
 import os
+import open3d as o3d
+import copy
+
 
 # Import the classes
 from colour_normalisation import ColourNorm
@@ -13,6 +16,7 @@ from backgroundRemoval import background
 from stereoMatching import stereoMatching
 from DepthMapCreator import DepthMapCreator
 from MeshGenerator import MeshGenerator  
+from ICPCombine import ICPCombine
 
 # Define the classes
 colournorm = ColourNorm()
@@ -21,6 +25,7 @@ background_removal = background()
 stereo_matching = stereoMatching()
 depth_map_creator = DepthMapCreator()
 mesh_generator = MeshGenerator()  
+icp_combine = ICPCombine()
 
 # Paths to calibration images
 calib_left = 'ipcv_project3/Calibratie 1/calibrationLeft'
@@ -36,6 +41,8 @@ subject_paths = [
     'ipcv_project3/subject2',
     'ipcv_project3/subject4'
 ]
+pcd_left_middle_list = []
+pcd_middle_right_list = []
 
 for subject_path in subject_paths:
     left_img_path = glob.glob(os.path.join(subject_path, 'subject*Left', '*.jpg'))[0]
@@ -47,6 +54,10 @@ for subject_path in subject_paths:
     left_img_bg_removed = background_removal.remove_background(left_img)
     middle_img_bg_removed = background_removal.remove_background(middle_img)
 
+    # convert color to RGB
+    left_img_bg_removed_RGB = cv.cvtColor(left_img_bg_removed, cv.COLOR_BGR2RGB)
+    middle_img_bg_removed_RGB = cv.cvtColor(middle_img_bg_removed, cv.COLOR_BGR2RGB)
+
     bg_removed_folder = os.path.join(subject_path, 'bg_removed')
     if not os.path.exists(bg_removed_folder):
         os.makedirs(bg_removed_folder)
@@ -54,8 +65,8 @@ for subject_path in subject_paths:
     left_img_bg_removed_path = os.path.join(bg_removed_folder, 'left_bg_removed.jpg')
     middle_img_bg_removed_path = os.path.join(bg_removed_folder, 'middle_bg_removed.jpg')
 
-    cv.imwrite(left_img_bg_removed_path, left_img_bg_removed)
-    cv.imwrite(middle_img_bg_removed_path, middle_img_bg_removed)
+    cv.imwrite(left_img_bg_removed_path, left_img_bg_removed_RGB)
+    cv.imwrite(middle_img_bg_removed_path, middle_img_bg_removed_RGB)
 
     rectified_left, rectified_middle, Q_left_middle = stereocal.stereo_rectification(
         CM1, dist1, CM2, dist2, R_left_middle, T_left_middle, left_img_bg_removed_path, middle_img_bg_removed_path
@@ -91,6 +102,14 @@ for subject_path in subject_paths:
         # 3D Mesh generation
         mesh_output_path = os.path.join(rectified_folder, '3d_mesh.png')
         mesh_generator.generate_mesh(depth_map, mesh_output_path)
+
+        # pcd creation
+        points_3d, valid_mask = depth_map_creator.create_3d_points(filtered_disparity_map, Q_left_middle)
+        colors = rectified_left[valid_mask].reshape(-1, 3) 
+        pcd_left_middle = depth_map_creator.create_3dpoint_cloud(points_3d, colors)
+        o3d.visualization.draw_geometries([pcd_left_middle], window_name="Colored Point Cloud with Normals")
+        pcd_left_middle_list.append(pcd_left_middle)
+
     else:
         print(f"Rectification failed for images: {left_img_path}, {middle_img_path}")
 
@@ -104,6 +123,10 @@ for subject_path in subject_paths:
     right_img_bg_removed = background_removal.remove_background(right_img)
     middle_img_bg_removed = background_removal.remove_background(middle_img)
 
+    # convert color to RGB
+    middle_img_bg_removed_RGB2 = cv.cvtColor(middle_img_bg_removed, cv.COLOR_BGR2RGB)
+    right_img_bg_removed_RGB2 = cv.cvtColor(right_img_bg_removed, cv.COLOR_BGR2RGB)
+
     bg_removed_folder = os.path.join(subject_path, 'bg_removed')
     if not os.path.exists(bg_removed_folder):
         os.makedirs(bg_removed_folder)
@@ -111,11 +134,11 @@ for subject_path in subject_paths:
     right_img_bg_removed_path = os.path.join(bg_removed_folder, 'right_bg_removed.jpg')
     middle_img_bg_removed_path = os.path.join(bg_removed_folder, 'middle_bg_removed.jpg')
 
-    cv.imwrite(right_img_bg_removed_path, right_img_bg_removed)
-    cv.imwrite(middle_img_bg_removed_path, middle_img_bg_removed)
+    cv.imwrite(right_img_bg_removed_path, right_img_bg_removed_RGB2)
+    cv.imwrite(middle_img_bg_removed_path, middle_img_bg_removed_RGB2)
 
     rectified_right, rectified_middle, Q_right_middle = stereocal.stereo_rectification(
-        CM1, dist1, CM2, dist2, R_middle_right, T_middle_right, right_img_bg_removed_path, middle_img_bg_removed_path
+        CM_middle, dist_middle, CM_right, dist_right, R_middle_right, T_middle_right, right_img_bg_removed_path, middle_img_bg_removed_path
     )
 
     if rectified_right is not None and rectified_middle is not None:
@@ -130,7 +153,7 @@ for subject_path in subject_paths:
         cv.imwrite(rectified_middle_path, rectified_middle)
 
         # Stereo matching
-        disparity_map = stereo_matching.stereoMatchingBM(rectified_right, rectified_middle)
+        disparity_map = stereo_matching.stereoMatchingBM(rectified_middle, rectified_right)
         unreliable_disparity_map = stereo_matching.unreliable_disparity_mask(disparity_map)
         filtered_disparity_map = stereo_matching.filter_disparity(disparity_map,unreliable_disparity_map)
 
@@ -148,5 +171,28 @@ for subject_path in subject_paths:
         # 3D Mesh generation
         mesh_output_path = os.path.join(rectified_folder, '3d_mesh.png')
         mesh_generator.generate_mesh(depth_map, mesh_output_path)
+
+        # pcd creation
+        points_3d, valid_mask = depth_map_creator.create_3d_points(filtered_disparity_map, Q_left_middle)
+        colors = rectified_middle[valid_mask].reshape(-1, 3) 
+        pcd_middle_right = depth_map_creator.create_3dpoint_cloud(points_3d, colors)
+        o3d.visualization.draw_geometries([pcd_middle_right], window_name="Colored Point Cloud with Normals")
+        pcd_middle_right_list.append(pcd_middle_right)
+
     else:
         print(f"Rectification failed for images: {left_img_path}, {middle_img_path}")
+
+for pcd_left, pcd_right in zip(pcd_left_middle_list, pcd_middle_right_list):
+    threshold = 0.05
+    # trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
+    #                     [-0.139, 0.967, -0.215, 0.7],
+    #                     [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
+    trans_init = np.eye(4)
+    reg_p2p = icp_combine.point_to_point_icp(pcd_right, pcd_left, threshold, trans_init)
+    print("Transformation Matrix:")
+    print(reg_p2p)
+
+    # 視覺化結果
+    icp_combine.draw_registration_result(pcd_right, pcd_left, reg_p2p)
+
+
